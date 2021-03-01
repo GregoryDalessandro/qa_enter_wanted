87b2a3269909271434122246c01104800dbadf9c
GregoryDalessandro <gdalessandro418@gmail.com>
2021-03-01 18:58:28 -0500

    pass page load test

--- src/PageObjects/enterWantedPage.ts
+++ src/PageObjects/enterWantedPage.ts
@@ -7,8 +7,16 @@ import {
 export class EnterWantedPage {
   driver: WebDriver;
   url: string = "https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html";
+  headerLogo: By = By.className("cake");
   constructor(driver: WebDriver) {
     this.driver = driver;
+  }
+  async navigate() {
+    await this.driver.get(this.url);
+    await this.driver.wait(until.elementLocated(this.headerLogo));
+    await this.driver.wait(
+      until.elementIsVisible(await this.driver.findElement(this.headerLogo))
+    );
   }
 }
 
