f10d18227c360cf0014e7905b9950358c0e09716
GregoryDalessandro <gdalessandro418@gmail.com>
2021-03-02 13:48:24 -0500

    pass first of five tests for Header Input field, and added TODO comments to implement later

--- src/PageObjects/enterWantedPage.ts
+++ src/PageObjects/enterWantedPage.ts
@@ -7,7 +7,25 @@ import {
 export class EnterWantedPage {
   driver: WebDriver;
   url: string = "https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html";
+  // selectors needed for testing:
   headerLogo: By = By.className("cake");
+  fieldHeader: By = By.name("hdrHeader");
+  headerInput: By = By.name("hdrInput");
+  submit: By = By.id("saveBtn");
+  // TODO Fix this hardcoded error message, move it to the test file
+  headerInputError: By = By.className("errorMessage");
+  /* TODO: fields to add for equivelance partitioning
+  MKE
+  Name
+  Hair
+  Offense
+  Date of Warrent/Violation
+  Driver License
+  Driver License Expiration Date
+  License Plate
+  Weight
+  */
+
   constructor(driver: WebDriver) {
     this.driver = driver;
   }
@@ -17,6 +35,11 @@ export class EnterWantedPage {
     await this.driver.wait(
       until.elementIsVisible(await this.driver.findElement(this.headerLogo))
     );
+  }
+  //this function allows user to test any input field, as there are a lot for Enter Wanted
+  async enterText(s: string) {
+    //await this.driver.findElement(this[`${inputFieldName}`]).sendKeys(s+`\n`);
+    await this.driver.findElement(this.headerInput).sendKeys(s+`\n`);
   }
 }
 
--- src/__tests__/enterWanted.test.ts
+++ src/__tests__/enterWanted.test.ts
@@ -1,5 +1,5 @@
 import { EnterWantedPage } from "../pageObjects/enterWantedPage";
-import { WebDriver, Builder, until, Capabilities } from "selenium-webdriver";
+import { WebDriver, Builder, until, Capabilities, By } from "selenium-webdriver";
 
 const chromedriver = require("chromedriver");
 const driver: WebDriver = new Builder()
@@ -17,9 +17,33 @@ describe("EnterWanted.com", () => {
   afterAll(async () => {
     await driver.quit();
   });
-  test("the page loads", async () => {
+  it("can load the main page", async () => {
     // wait for the page to load, making sure we can see the header logo
     await driver.wait(until.elementLocated(page.headerLogo));
     // make sure the header's inner text is Enter Wanted
-    expect(await (await driver.findElement(page.headerLogo)).getText()).toEqual("Enter Wanted") });
+    expect(await (await driver.findElement(page.headerLogo)).getText()).toEqual("Enter Wanted")
+  });
+  /* Jira Test Case for the following test:
+    https://dmutah.atlassian.net/browse/GDDL-43
+  */
+  it("will give the user an error message if the Header input is out of range", async () => {
+    await driver.wait(until.elementLocated(page.headerLogo));
+  // test min - 1 value (bad)
+    // select the Header's input field
+    // enter text "a23$5678"
+    await page.enterText("a23$5678");
+    // click submit
+    await page.driver.findElement(page.submit).click();
+    // verify there is an error message
+    let error = await (await page.driver.findElement(By.xpath('(//li[@class="errorMessage"])[1]'))).getText();
+    expect(error).toEqual('The "Header" field should be between 9 and 19 characters long.');
+    /*TODO: create seperate expects for lines 42 - 45 to be housed within the single it test on line 29
+     need to make lines 30, 34, and 36 modular so that they don't affect the other expects for lines 42-45
+    // test min value (good)
+    // test max value (good)
+    // test max -1 value (good)
+    // test max + 1 value (bad)
+    */
+  });
 });
+
